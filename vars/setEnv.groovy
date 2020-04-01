e53de952813390488e0185f52a545bff4dc99acd
ypenn21 <yannipeng21@gmail.com>
2020-03-31 23:14:21 -0700

    using @groovy.transform.Field

--- vars/setEnv.groovy
+++ vars/setEnv.groovy
@@ -1,6 +1,7 @@
 def call() {
     env.APP_NAME = "spring_pet_clinic"
     env.TAG = "latest"
+    env.YANNI = "YANNI"
 
     echo "env.APP_NAME set to: ${env.APP_NAME}"
     echo "env.TAG set to: ${env.TAG}"
